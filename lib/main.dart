1089ac9911216535257d6fc5924a7570a8d0a86f
hashimys <hashimys1991@gmail.com>
2024-08-01 11:42:50 +0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,41 @@
+import 'package:education_app/core/common/app/providers/user_provider.dart';
+import 'package:education_app/core/res/colours.dart';
+import 'package:education_app/core/res/fonts.dart';
+import 'package:education_app/core/services/injection_container.dart';
+import 'package:education_app/core/services/router.dart';
+import 'package:education_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await init();
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MaterialApp(
+        title: 'Education App',
+        theme: ThemeData(
+          useMaterial3: true,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: Fonts.poppins,
+          appBarTheme: const AppBarTheme(
+            color: Colors.transparent,
+          ),
+          colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
         ),
+        onGenerateRoute: generateRoute,
       ),
     );
   }
